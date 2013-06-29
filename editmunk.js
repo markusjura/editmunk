dc6f6116d52ad45a8b2c20c91c7cfe6bf2340558
Mashhood Rastgar <mashhoodr@gmail.com>
2013-06-29 16:40:00 -0700

    styled the editor + fixed the rendering of the editor + refactoring

--- editmunk.js
+++ editmunk.js
@@ -1,23 +1,6 @@
+
 if (Meteor.isClient) {
-  Meteor.startup(function () {
-    $(document).ready(function (){
-      var editor = ace.edit("editor");
-      editor.setTheme("ace/theme/monokai");
-      editor.getSession().setMode("ace/mode/javascript");
-    });
-  });
 
-  Template.hello.greeting = function () {
-    return "Welcome to editmunk!";
-  };
-
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
 }
 
 if (Meteor.isServer) {
--- main.js
+++ main.js
@@ -1,5 +1,24 @@
+function render_editor() {
+  if($('#editor').length > 0) {
+    var editor = ace.edit("editor");
+    editor.setTheme("ace/theme/xcode");
+    editor.getSession().setMode("ace/mode/javascript");
+  }
+}
+
+
 if (Meteor.is_client) {
+  Meteor.startup(function () {
+    $(document).ready(function (){
+      render_editor();
+    });
+  });
+
   Template.main.loggedIn = function () {
+    setTimeout(function() {
+        render_editor();
+      }, 100);
+
     return Meteor.userId();
   };
 }
